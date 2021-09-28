1c581f21cdd3ce0a15acff29ba0eb192de639645
Allan Haggett <allan.haggett@gov.bc.ca>
2021-09-27 21:50:43 -0700

    Reformat course index; start putting alphabet back

--- archive-course.php
+++ archive-course.php
@@ -38,58 +38,79 @@ $categories = get_terms(
     array('parent' => 0)
 );
 ?>
-<div class="dark-wrap">
-<div class="wp-block-columns alignwide" id="pagetop">
-<div class="wp-block-column" style="flex-basis:33.33%">
-<figure class="post-thumbnail">
-    <img src="https://lc.virtuallearn.ca/portal/wp-content/uploads/sites/6/2021/06/hero-2.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" srcset="https://lc.virtuallearn.ca/portal/wp-content/uploads/sites/6/2021/06/hero-2.jpg 888w, https://lc.virtuallearn.ca/portal/wp-content/uploads/sites/6/2021/06/hero-2-300x225.jpg 300w, https://lc.virtuallearn.ca/portal/wp-content/uploads/sites/6/2021/06/hero-2-768x575.jpg 768w" sizes="(max-width: 888px) 100vw, 888px" style="width:100%;height:74.89%;max-width:888px;" width="888" height="665">
-</figure>
-</div>
-<div class="wp-block-column">
-    <div style="padding: 2em 0;">
-
-    <h1>Courses</h1>
-    <div style="padding: 1em 0;">
-    <?php echo $post_my_query->post_count; ?> 
-    courses in 3 top-level categories from 14 
-    <a href="/portal/corporate-learning-partners/" style="color: #FFF">Learning Partners</a>
-    </div>
-    <div>
-    <?php foreach( $categories as $category ): ?>
-    <?php if($category->name == "Ministry") continue ?>
-    <?php 
-    $category_link = sprintf( 
-        '<a href="%1$s" alt="%2$s" class="coursecat">%3$s</a>',
-        esc_url( get_category_link( $category->term_id ) ),
-        esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
-        esc_html( $category->name )
-    );
-    ?>
-
-    <?= sprintf( esc_html__( '%s', 'textdomain' ), $category_link ) . ' ' ?>
 
 
-    <?php endforeach ?>
-</div>
-</div>
-</div>
-</div>
-</div>
+
+<style>
+    .alphabet a {
+        background: #F1F1F1;
+        border-radius: 3px;
+        display: inline-block;
+        font-size: .6em;
+        margin: 0 .15em;
+        padding: .25em .5em;
+        text-decoration: none;
+    }
+    </style>
+    <div class="alphabet text-center">
+        <a href="#A">A</a>
+        <a href="#B">B</a>
+        <a href="#C">C</a>
+        <a href="#D">D</a>
+        <a href="#E">E</a>
+        <a href="#F">F</a>
+        <a href="#G">G</a>
+        <a href="#H">H</a>
+        <a href="#I">I</a>
+        <a href="#J">J</a>
+        <a href="#K">K</a>
+        <a href="#L">L</a>
+        <a href="#M">M</a>
+        <a href="#N">N</a>
+        <a href="#O">O</a>
+        <a href="#P">P</a>
+        <a href="#Q">Q</a>
+        <a href="#R">R</a>
+        <a href="#S">S</a>
+        <a href="#T">T</a>
+        <a href="#U">U</a>
+        <a href="#V">V</a>
+        <a href="#W">W</a>
+        <a href="#X">X</a>
+        <a href="#Y">Y</a>
+        <a href="#Z">Z</a>
+    </div> <!-- /.alphabet -->
 
 <div class="entry-content" id="courselist" style="margin-top: 1em;">
 <div class="searchbox">
-<input class="search form-control mb-3" placeholder="Search">
-<!-- <div><span id="coursecount"></span> courses</div> -->
-
+<input class="search form-control mb-3" placeholder="Filter">
+<div>
+    <span id="coursecount"><?php echo $post_my_query->post_count; ?></span>
+    courses
+</div>
+</div>
 <?php if( $post_my_query->have_posts() ) : ?>
 
-</div>    
 <div class="entry-content">
 <div class="list">
 <?php
+$lastletter = '';
 while ($post_my_query->have_posts()) : $post_my_query->the_post(); 
 
+$title = get_the_title();
+$firstletter = substr($title, 0, 1);
+$secondletter = substr($title, 0, 2);
+
+if($firstletter != '{' && $firstletter != '(') {            
+    if($firstletter != $lastletter) {
+        // not sure what to do here as list.js is counting these headers as courses :(
+        //echo '<h2 id="' . $firstletter . '">' . $firstletter . '</h2>';
+    }
+}
+
 get_template_part( 'template-parts/course/single-course' );
+
+$lastletter = $firstletter;
 
 endwhile;
 ?>
