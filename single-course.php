2cd093942c3b21c4a289b113620d178ec936d069
Allan Haggett <allan.haggett@gov.bc.ca>
2021-10-07 19:10:36 -0700

    Better single course view

--- single-course.php
+++ single-course.php
@@ -30,37 +30,37 @@ while ( have_posts() ) :
 </div>
 </div>
 
-<div class="entry-content">
-<div class="course">
+<div class="entry-content my-1">
+<div class="course bg-white">
 <div style="background: #28537d; height: 6px; width: 100%;"></div> 
+<div class="p-1">
+    <div class="details" id="course-<?= $post->ID ?>">
 
+        <div class="coursedesc bg-white mt-wee">
+            <?php the_content(); ?>
+        </div>
+        <div class="learningpartner mt-wee">
+            <?php the_terms( $post->ID, 'learning_partner', 'Offered by: ', ', ', ' ' ); ?>
+        </div>
+        <div class="courseregister my-1">
+        <?php $exsys = get_the_terms( $post->ID, 'external_system', '', ', ', ' ' ) ?>
+        <a class="registerbutton" 
+            href="<?= $post->course_link ?>" 
+            target="_blank" 
+            rel="noopener">
+                Register on <?= $exsys[0]->name ?>
+        </a>
+        </div>
+        <div class="coursecats mt-1">
+            <?php the_terms( $post->ID, 'course_category', 'Categories: ', ', ', ' ' ); ?>
+        </div>
+        <div class="delivery-method">
+            <?php the_terms( $post->ID, 'delivery_method', 'Delivery Method: ', ', ', ' ' ); ?>
+        </div>
 
-<div class="learningpartner my-1">
-	<?php the_terms( $post->ID, 'learning_partner', 'Learning Partner: ', ', ', ' ' ); ?>
-</div>
-
-<div class="coursedesc p-1 bg-white">
-	<?php the_content() ?>
-</div>
-<div class="mt-1">
-	<a style="background: #3a9bd9; color: #F2F2F2; display: block; font-size: 2rem; padding: .25em .5em; text-align: center" 
-		href="<?= $post->course_link ?>" 
-		target="_blank" 
-		rel="noopener">
-			Register Here
-	</a>
-	</div>
-
-	<div class="bg-white my-1 p-1">
-	<div class="coursecats"><?php the_terms( $post->ID, 'course_category', 'Categories: ', ', ', ' ' ); ?></div>
-	<div class="delivery-method"><?php the_terms( $post->ID, 'delivery_method', 'Delivery Methods: ', ', ', ' ' ); ?></div>
-	<div><?php the_terms( $post->ID, 'role', 'Roles: ', ', ', ' ' ); ?></div>
-	<div style="display:none"><?php the_terms( $post->ID, 'keywords', 'Keywords: ', ', ', ' ' ); ?></div>
-	</div>
-
-
-
-</div><!-- .course -->
+    </div>
+    </div>
+</div> <!-- /.course -->
 </div><!-- .entry-content -->
 
 <?php endwhile; // End of the loop.
