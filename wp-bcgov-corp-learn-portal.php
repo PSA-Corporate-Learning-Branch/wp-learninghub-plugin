96d89ad1831b6d3f255f14f69e90e60d887a9a2e
Allan Haggett <47408983+allanhaggett@users.noreply.github.com>
2022-05-02 13:56:46 -0700

    Fix logic around saving course_link custom field via metabox

--- wp-bcgov-corp-learn-portal.php
+++ wp-bcgov-corp-learn-portal.php
@@ -594,13 +594,13 @@ function course_save_course_link_meta ( $post_id, $post ) {
     $meta_value = get_post_meta( $post_id, $meta_key, true );
 
     /* If a new meta value was added and there was no previous value, add it. */
-    if ( $new_meta_value && ’ == $meta_value ) {
+    if ( $new_meta_value && !$meta_value ) {
         add_post_meta( $post_id, $meta_key, $new_meta_value, true );
     /* If the new meta value does not match the old value, update it. */
     } elseif ( $new_meta_value && $new_meta_value != $meta_value ) {
         update_post_meta( $post_id, $meta_key, $new_meta_value );
     /* If there is no new meta value but an old value exists, delete it. */
-    } elseif ( ’ == $new_meta_value && $meta_value ) {
+    } elseif ( !$new_meta_value && $meta_value ) {
         delete_post_meta( $post_id, $meta_key, $meta_value );
     }
 }
